46053a1808cd3a56832451e837188ed5e7f54295
Ricardo <cadym1997@gmail.com>
2023-04-25 23:29:30 -0300

    Adicionando mais dois tipos de funções

---
+++ funcoes/variatica/variatica.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func media(numeros ...float64) float64 {
+	total := 0.0
+	for _, num := range numeros {
+		total += num
+	}
+	return total / float64(len(numeros))
+}
+
+func main() {
+	fmt.Printf("Média: %.2f", media(9.6, 5.9, 8.9, 6.9))
+}
